48e868b25bb58395f5c336ebfef07d7794386571
Rebecca Simon <Simon.RebeccaL@gmail.com>
2016-04-19 22:15:57 -0400

    The Bartendress : first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('appApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Home',
+      'About',
+      'Recipes'
+    ];
+  });
